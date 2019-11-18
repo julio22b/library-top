b098767bc9e1b7bbefcecf3f06df41432194a6f2
julio22b <jbermudezfc90@gmail.com>
2019-11-18 15:23:54 -0400

    changed the constructor to a class

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-let book1 = {
-    title: 'A Game of Thrones',
-    author: 'George R.R. Martin',
-    numberOfPages: 782,
-    hasBeenRead: 'Read'
+class Book {
+    constructor(title, author, numberOfPages, hasBeenRead){
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.hasBeenRead = hasBeenRead;
+    }
+
+    changeReadStatus(){
+        this.hasBeenRead === 'Read' ? this.hasBeenRead = 'Not read' : this.hasBeenRead = 'Read'
+    }
 }
 
-let book2 = {
-    title: 'One Hundred Years of Solitude',
-    author: 'Gabriel García Márquez',
-    numberOfPages: 448,
-    hasBeenRead: 'Not read'
-}
+const book1 = new Book('A Game of Thrones', 'George R.R Martin', 782, 'Read')
+const book2 = new Book('One Hundred Years of Solitude', 'Gabriel García Márquez', 448, 'Not read')
 
 let addBook = [book2,book1]
 let myLibrary = [book2,book1]
-
-function Book(title, author, numberOfPages, hasBeenRead){
-    this.title = title
-    this.author = author
-    this.numberOfPages = numberOfPages
-    this.hasBeenRead = hasBeenRead
-}
-
-Book.prototype.changeReadStatus = function(){
-    if(this.hasBeenRead === 'Read'){
-        return this.hasBeenRead = 'Not read'
-    } else {
-        return this.hasBeenRead = 'Read'
-    }
-}
 
 const inputs = document.getElementsByClassName('input')
 
@@ -44,7 +31,9 @@ function addBookToLibrary(){
     let numberOfPages = inputs[2].value
     let hasBeenRead = inputs[3].value
     if(!title){
-        return (alert('At least the title must be added.'))
+        return alert('At least a title must be added.')
+    } else if(typeof numberOfPages === 'string'){
+        return alert('Specify a number of pages')
     }
     let newBook = new Book(title, author, numberOfPages, hasBeenRead)
     
